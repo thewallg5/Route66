7654a90cacc8e33dcf85aea593bb2a0f2fc72216
Gabriele Vellei <43831310+thewallg5@users.noreply.github.com>
2019-01-17 13:39:43 +0100

    Add files via upload

---
+++ MainServer/auth.js
@@ -0,0 +1,118 @@
+
+// FUNZIONI AUTH,CHIAMATE DAL MAINSERVER
+
+
+// in the parameters, social_id means 'twt', 'tmb', 'fkr' ecc.., while social_name is the
+// regular name, like 'twitter', 'tumblr'. It's useful when redirecing to main server,
+// e.g.: "/auth/register_access/"+social_name
+
+var fs = require('fs')
+var globals = require('./globals.js')
+var req_list = globals.req_list
+var log = globals.log
+
+
+
+/* tokens that will be used to verify pin are saved in req_list. token1 and token2 are URLencoded
+ */
+function start(social_id, req, res) {
+  var t1 = req.query.token1
+  var t2 = req.query.token2
+  
+  log('Saving request tokens for '+social_id+' authentication ')
+  
+  if (typeof(t1)=='undefined' || typeof(t2)=='undefined'){
+    res.send({result:"no", msg:'Bad request body while saving twitter request tokens to cookies'})
+    return
+  }
+  
+  req_list[social_id][t1] = t2
+  
+  res.send('<html> <script>console.log("yes, saved request tokens to req_list for '+social_id+'"); '+
+            'window.close();</script> </html>')
+}
+
+
+
+
+/* OAuth redirect page
+ */
+function oauth_landing(social_id, social_name, req, res) {
+  var pin = req.query.oauth_verifier
+  var token1 = req.query.oauth_token
+  
+  log('Getting pin from list for '+social_id+' authentication')
+  
+  if (typeof(pin)=='undefined' || typeof(token1)=='undefined'){
+    res.send({result:"no", msg:'Bad request params while getting from URL'})
+    return
+  }
+  
+  var token2 = req_list[social_id][token1]
+  if (typeof(token2)=='undefined') {
+    res.send({result:"no", msg:'Bad value in req_list while getting '+social_id+' request tokens'})
+    return
+  }
+  
+  // Deleting entry token1:token2 from req_list
+  delete req_list[social_id][token1]
+  
+  fs.readFile('MainServer/res/auth.html', 'utf8', function (err,data) {
+    if (err) { return console.log(err) }
+    var result = data.replace("<!--TOKEN_1-PLACEHOLDER-->", token1)
+    result = result.replace("<!--TOKEN_2-PLACEHOLDER-->", token2)
+    result = result.replace("<!--SOCIAL-PLACEHOLDER-->", social_name)
+    result = result.replace("<!--PIN-PLACEHOLDER-->", pin)
+    result = result.replace("<!--WEBSOCKET-PORT-PLACEHOLDER-->", globals.ws_ports[social_id])
+    res.send(result)
+  })
+}
+
+
+
+
+/* After a successful auth the server register access tokens in cookies. 
+ *   request body (JSON):
+ *   {
+ *      token1: 'token1',
+ *      token2: 'token2'
+ *   }
+*/
+function register_access(social_id, req, res) {
+  var t1 = req.body.token1
+  var t2 = req.body.token2
+
+  log('Registering access to '+social_id)
+
+  if (typeof(t1)=='undefined' || typeof(t2)=='undefined'){
+    res.send({result:"no", msg:'Bad request body while registering access'})
+    return
+  }
+
+  // If there are not cookies create a new one
+  var cookie = req.cookies.route
+  if (typeof(cookie)=='undefined' || typeof(cookie.logged)=='undefined' ) {
+    cookie = {}
+    cookie.logged = []
+  }
+  
+  // Add new social field to cookie
+  cookie[social_id] = {}
+  cookie[social_id].token1 = t1
+  cookie[social_id].token2 = t2
+  if (!cookie.logged.includes(social_id))
+    cookie.logged.push(social_id)
+  res.cookie('route', cookie)
+  
+  res.send('<html> <script>console.log("yes, registered to '+social_id+'"); '+
+            'window.close();</script> </html>')
+}
+
+
+
+// esporto le funzioni
+module.exports = {
+  start,
+  oauth_landing,
+  register_access
+}
---
+++ MainServer/upload_post.js
@@ -0,0 +1,120 @@
+// CHIAMATO DAL MAINSERVER
+
+// si occupa di caricare il post facendo i controlli necessari e inoltrando alle code
+
+var queue = require('./queues.js')
+var globals = require('./globals.js')
+var log = globals.log
+
+
+
+
+/* User POSTs when he wants to upload a post
+ *
+ * The body of the POST made by the user it's multipart/form-data encoded
+ * 
+ *   'data' = text of the post 
+ *   'twt'  = 'on' if selected
+ *   'tmb'  = 'on' if selected
+ *   'fkr'  = 'on' if selected
+ *   'image' = path
+ *
+ * 
+ * where twt is 'on' if the user wants to post to twitter, tmb if he wants
+ * to post to tumbrl and flk for flickr
+ * 
+ * 'image' is optional, is the path of the image. If there's no image then
+ * it's an empty string ""
+ */
+function upload_post(req, res) {
+  var text = req.body.text
+ 
+  // Build list from single fields
+  var list = []
+  if (req.body.twt == 'on') list.push('twt')
+  if (req.body.tmb == 'on') list.push('tmb')
+  if (req.body.fkr == 'on') list.push('fkr')
+  
+  var image = req.file
+  if (typeof(image)=='undefined')
+    image = ""
+  
+  log('Received text:'+text+' list:'+list+' image:'+(image.length>0).toString())
+
+  // sanity check, list and text can't be undefined
+  if (typeof(text) == 'undefined' || typeof(list) == 'undefined') {
+    res.status(400)   //bad request
+    res.send({result:'no', msg:"The received POST didn't have correct parameters in the body"})
+    return
+  }
+
+  text = text.trim()   //removing leading and trailing spaces
+  
+  // sanity check, filter all social not listed in RPC_FORMAT.md
+  //list = list.filter(function(e){ return !e in ['fb','twt','g+','tmb'] })
+
+  // sanity check, if list is empty i don't publish on any social network
+  if (list.length == 0) {
+    res.send({result:'no', msg:'no social selected'})
+    return
+  }
+  
+  // sanity check, text must be non-empty
+  if (text.length == 0) {
+    res.send({result:'no', msg:'text can\'t be empty'})
+    return
+  }
+
+  // sanity check, if user post contains utf char '\xFF' 'ÿ', substitute the 'ÿ' with a 'y'
+  text = text.replace(new RegExp('\xFF', 'g'), 'y')
+ 
+  
+  // C00kies sanity checks
+  cookie = req.cookies.route
+  var need_auth = []
+  list.forEach( function(network) {
+    if (typeof(cookie)=='undefined' || typeof(cookie.logged)=='undefined' ||
+            !cookie.logged.includes(network))
+      need_auth.push(network)
+  })
+  
+  // If cookie fails sanity check
+  if (need_auth.length > 0) {
+    res.send({result:"no", msg:"Need auth to "+need_auth, auth:need_auth})
+    return
+  }
+
+
+  var token1 = []
+  var token2 = []
+  var exception = false
+  list.forEach( function(network) {
+    try {
+      token1.push(cookie[network].token1)
+      token2.push(cookie[network].token2)
+    } catch (ex) {
+      exception = true
+      res.send({result:"no", msg:"Need auth", auth:[network]})
+      return
+    }
+  })
+  
+  if (exception)
+    return
+
+  // if the program is here we have a token, proceed to upload post
+  // (Following RPC syntax in RPC_FORMAT.md)
+  for (var i=0; i<list.length; i++) {
+    var msg = ['upload_post', token1[i], token2[i], text, image.path].join('\xFF')
+    if (list[i]=='fkr' && image=='')
+      log('Trying to send to flickr only text, skipping')
+    else
+	  queue.send(msg, list[i])
+  }
+
+  res.send({result:"yes", msg:"forwarded to "+list})
+}
+
+module.exports = {
+  upload_post
+}
