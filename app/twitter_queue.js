b5e055ac7a2b2908a88a987118890148ff70485e
Gabriele Vellei <43831310+thewallg5@users.noreply.github.com>
2019-05-07 12:06:16 +0200

    Add files via upload

--- app/twitter_queue.js
+++ app/twitter_queue.js
@@ -3,15 +3,15 @@ var twt = require("./twitter.js");
 var base64Img = require('base64-img');
 var fs = require('file-system');
 
-amqp.connect('amqp://localhost', function(err, conn) {
-	conn.createChannel(function(err, ch) {
+amqp.connect('amqp://localhost', function(err, conn) {//apriamo una connessione
+	conn.createChannel(function(err, ch) {	//e un canale 'ch'
 		var ex = 'exchg';
 
-		ch.assertExchange(ex, 'direct', { durable: true });
-		ch.assertQueue('', { exclusive: true }, function(err, q) {
-			ch.bindQueue(q.queue, ex, 'twt');
+		ch.assertExchange(ex, 'direct', { durable: true });  //un messaggio va alle code la cui binding key corrisponde esattamente alla routing key del messaggio
+		ch.assertQueue('', { exclusive: true }, function(err, q) { //exclusive=true-> estende la coda alla connessione; se vuoto '' il server ne creerà una automaticamente
+			ch.bindQueue(q.queue, ex, 'twt'); //lo scambio 'ex' inoltrerà il msg alla queue 'q.queue' denominata, in base al tipo di scambio e al modello 'twt' indicato
 
-			ch.consume(q.queue, function(msg) {
+			ch.consume(q.queue, function(msg) {  //Imposta un consumatore con una callback da richiamare con ogni messaggio.
                 console.log("msg received by twt queue");
 				var l = msg.content.toString().split('0xFF');
                 console.log(l);
@@ -27,7 +27,6 @@ amqp.connect('amqp://localhost', function(err, conn) {
                 console.log("checking img");
 
                 if(l[1].length > 0) {
-
                     var data = fs.readFileSync(__dirname + '/' + l[1], { encoding: 'base64' });
 
                     console.log("Uploading img...");
@@ -40,7 +39,7 @@ amqp.connect('amqp://localhost', function(err, conn) {
 
 
                             var params = {
-                                'media_ids': new Array(media_id),
+                                'media_ids': media_id,
                                 'status': l[0]
 
                             };
@@ -48,7 +47,7 @@ amqp.connect('amqp://localhost', function(err, conn) {
                             console.log(params);
 
                             console.log("Ready 2 post");
-                            client.post(params, 'https://api.twitter.com/1.1/statuses/update.json', function(err, tweet, response) {
+                            client.post(params, 'https://api.twitter.com/1.1/statuses/update.json', function(err, tweet, response) { //?media_id='+media_id
                                 if(err) {
                                     console.log(err);
                                 }
@@ -69,13 +68,13 @@ amqp.connect('amqp://localhost', function(err, conn) {
                         if(err) {
                             console.log(err);
                         }
-                        else {
+                        else { 
                             console.log("Posted successfully on Twitter");
                         }
                     });
                 }
 
-			}, { noAck: true });
+			}, { noAck: true }); //se è vero, il broker non si aspetta un riconoscimento dei messaggi consegnati a questo consumatore
 		});
 	});
 });
